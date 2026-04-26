ccfdc8eeaf9e87fd5fa0e3a77f516a74d6b8dacc
Mark Malstrom <mark@malstrom.me>
2026-04-26 15:33:51 -0500

    CheckerDivider component

---
+++ docs/.vitepress/theme/components/CheckerDivider.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="checker-wrap">
+        <span class="checker-divider" />
+    </div>
+</template>
+
+<style scoped>
+.checker-wrap {
+    display: block;
+    width: 100%;
+    padding: 0;
+    line-height: 0;
+}
+</style>
