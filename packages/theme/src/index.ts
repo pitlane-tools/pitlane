e22c328d4667c264d05180324c8b948cdc460325
Mark Malstrom <mark@malstrom.me>
2026-07-08 00:07:57 -0500

    Export the @pitlane/theme public API

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,1 +1,24 @@
-export const VERSION = "0.1.0";
+export { createTheme } from "./theme.ts";
+export type { ThemeComponent, ThemeOptions, ThemeProps, ThemeResult } from "./theme.ts";
+export { css } from "./css.ts";
+export { combine, cx, tva } from "./tva.ts";
+export type { ClassValue, CombinedTVAFn, TVAConfig, TVAFn, TVAProps } from "./tva.ts";
+export { ThemeError } from "./tokens.ts";
+export type { ThemedCSSProps } from "./props.ts";
+export type {
+    AnyToken,
+    BorderToken,
+    ColorToken,
+    CubicBezierToken,
+    DimensionToken,
+    DurationToken,
+    FontFamilyToken,
+    FontWeightToken,
+    GradientToken,
+    NumberToken,
+    ShadowToken,
+    StrokeStyleToken,
+    TokenType,
+    TransitionToken,
+} from "./brands.ts";
+export type { DeepPartialTokens, DTCGDocument, TokenTree } from "./types.ts";
